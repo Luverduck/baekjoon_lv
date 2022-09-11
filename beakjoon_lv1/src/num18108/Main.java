d18c95b198e0cb44f3770825a51bf8baf5cea06f
Luverduck <hyekko92@gmail.com>
2022-09-11 23:11:25 +0900

    9단계 : 1998년생인 내가 태국에서는 2541년생?!

---
+++ beakjoon_lv1/src/num18108/Main.java
@@ -0,0 +1,17 @@
+package num18108;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		System.out.println(input - 543);
+		
+		sc.close();
+	}
+}
